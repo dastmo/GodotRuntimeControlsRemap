7c4346ceca6996bea385649e7a38104c9b4e9fed
Damyan Momchev <dastmo@gmail.com>
2026-07-10 14:37:12 +0300

    Update controls_remap.gd

--- addons/runtime_controls_remap/controls_remap.gd
+++ addons/runtime_controls_remap/controls_remap.gd
@@ -1,20 +1,34 @@
 extends Node
 
 
+const SETTING_PATH: String = "addons/Controls Remap/Remappable Actions"
+const EVENTS_TEXT_DICTIONARY_PATH: String = "addons/Controls Remap/Events Dictionary"
+
+
+var _action_remap_requested: StringName = &""
+
+
 var _default_controls: Dictionary[StringName, Array] = {}
 var _customized_controls: Dictionary[StringName, Array] = {}
 var _file_path: String = "user://custom_controls.dat"
-var _setting_path: String = "addons/Controls Remap/Remappable Actions"
+
+
+signal control_remapped(action: StringName)
 
 
 func _enter_tree() -> void:
 	_record_default_controls()
 	_load_customized_controls()
+	
+	for action in _customized_controls:
+		InputMap.action_erase_events(action)
+		for event in _customized_controls[action]:
+			InputMap.action_add_event(action, event)
 
 
 func _record_default_controls() -> void:
 	for action in InputMap.get_actions():
-		if not ProjectSettings.get_setting(_setting_path).has(action):
+		if not ProjectSettings.get_setting(SETTING_PATH).has(action):
 			continue
 		
 		if OS.has_feature("editor"):
@@ -30,11 +44,13 @@ func _load_customized_controls() -> void:
 	var file = FileAccess.open(_file_path, FileAccess.READ)
 	
 	if file == null:
-		print(file.get_open_error())
+		printerr(file.get_open_error())
 		return
 	
 	_customized_controls = file.get_var(true)
 	file.close()
+	
+	control_remapped.emit(&"")
 
 
 func _save_cutomized_controls() -> void:
@@ -46,8 +62,11 @@ func _save_cutomized_controls() -> void:
 func remap_input(action: StringName, event: InputEvent) -> void:
 	if (event is InputEventKey) or (event is InputEventMouseButton):
 		_remap_keyboard_input(action, event)
-	elif event is InputEventJoypadButton:
+	elif (event is InputEventJoypadButton) or (event is InputEventJoypadMotion):
 		_remap_joypad_input(action, event)
+	
+	control_remapped.emit(action)
+	_action_remap_requested = &""
 
 
 func _remap_keyboard_input(action: StringName, event: InputEvent) -> void:
@@ -68,7 +87,7 @@ func _remap_keyboard_input(action: StringName, event: InputEvent) -> void:
 
 func _remap_joypad_input(action: StringName, event: InputEvent) -> void:
 	for old_event in InputMap.action_get_events(action):
-		if old_event is InputEventJoypadButton:
+		if (old_event is InputEventJoypadButton) or (old_event is InputEventJoypadMotion):
 			InputMap.action_erase_event(action, old_event)
 			InputMap.action_add_event(action, event)
 			
@@ -83,7 +102,7 @@ func _remap_joypad_input(action: StringName, event: InputEvent) -> void:
 
 
 func _restore_all_default_inputs() -> void:
-	for action in ProjectSettings.get_setting(_setting_path):
+	for action in ProjectSettings.get_setting(SETTING_PATH):
 		_restore_default_input(action, false)
 	
 	_save_cutomized_controls()
@@ -101,3 +120,15 @@ func _restore_default_input(action: StringName, save_controls: bool = true) -> v
 	
 	if save_controls:
 		_save_cutomized_controls()
+
+
+func is_action_remappable(action: StringName) -> bool:
+	return ProjectSettings.get_setting(SETTING_PATH).has(action)
+
+
+func get_button_text(event: InputEvent) -> String:
+	var dict: Dictionary = ProjectSettings.get_setting(EVENTS_TEXT_DICTIONARY_PATH)
+	if dict.has(event.as_text()):
+		return dict[event.as_text()]
+	else:
+		return event.as_text()
